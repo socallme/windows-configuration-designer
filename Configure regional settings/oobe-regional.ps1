20fcc95b5de69dc7c50678dd5c1b32964ff6cd00
socallme <140671519+socallme@users.noreply.github.com>
2024-10-08 13:28:54 +1100

    Update oobe-regional.ps1
    Aus

--- Configure regional settings/oobe-regional.ps1
+++ Configure regional settings/oobe-regional.ps1
@@ -1,14 +1,14 @@
 # configure time zone
-Get-TimeZone -ListAvailable | ?{$_.DisplayName -like "*Budapest*"} | Set-TimeZone
+Get-TimeZone -ListAvailable | ?{$_.DisplayName -like "*Australia*"} | Set-TimeZone
 
 # configure regional/locale/keyboard settings
 # Hungarian
-$region = "hu-HU"
+$region = "en-AU"
 
 Set-Culture $region
 Set-WinSystemLocale $region
 Set-WinUserLanguageList $region, "en-us" -force -wa silentlycontinue
-Set-WinHomeLocation 109
+Set-WinHomeLocation 12
 
 # copy regional settings to new user accounts and welcome screen
 Copy-UserInternationalSettingsToSystem -WelcomeScreen $True -NewUser $True
